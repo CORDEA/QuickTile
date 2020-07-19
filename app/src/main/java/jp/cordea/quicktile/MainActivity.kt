be1ead9ca944d2d6a9d65d8122c4b593b79b3752
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-19 17:41:59 +0900

    inject viewmodel

--- app/src/main/java/jp/cordea/quicktile/MainActivity.kt
+++ app/src/main/java/jp/cordea/quicktile/MainActivity.kt
@@ -1,11 +1,14 @@
 package jp.cordea.quicktile
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

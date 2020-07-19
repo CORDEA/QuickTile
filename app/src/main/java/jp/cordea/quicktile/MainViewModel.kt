d2ec36bebd46278110301751b7b1a43b3e5605d8
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-19 18:49:28 +0900

    update

--- app/src/main/java/jp/cordea/quicktile/MainViewModel.kt
+++ app/src/main/java/jp/cordea/quicktile/MainViewModel.kt
@@ -1,15 +1,21 @@
 package jp.cordea.quicktile
 
+import android.service.quicksettings.Tile
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 
-class MainViewModel @ViewModelInject constructor() : ViewModel() {
+class MainViewModel @ViewModelInject constructor(
+    private val repository: TileStatusRepository
+) : ViewModel() {
     fun onActiveClick() {
+        repository.update(Tile.STATE_ACTIVE)
     }
 
     fun onInactiveClick() {
+        repository.update(Tile.STATE_INACTIVE)
     }
 
     fun onUnavailableClick() {
+        repository.update(Tile.STATE_UNAVAILABLE)
     }
 }

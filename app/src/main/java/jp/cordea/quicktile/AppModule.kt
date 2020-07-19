8409b94202903af1a1ee41fab0ee8fdf1ee4a1de
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-19 16:15:03 +0900

    provide preferences

---
+++ app/src/main/java/jp/cordea/quicktile/AppModule.kt
@@ -0,0 +1,20 @@
+package jp.cordea.quicktile
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+private const val PREFERENCE_NAME = "jp.cordea.quicktile.pref"
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class AppModule {
+    @Provides
+    @Reusable
+    fun providePreferences(context: Context): SharedPreferences =
+        context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+}

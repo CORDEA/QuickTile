82348bc1aec2931ee7beb7bc24149d6827bbab47
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-19 17:33:10 +0900

    add main viewmodel

---
+++ app/src/main/java/jp/cordea/quicktile/MainViewModel.kt
@@ -0,0 +1,6 @@
+package jp.cordea.quicktile
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class MainViewModel @ViewModelInject constructor() : ViewModel()
